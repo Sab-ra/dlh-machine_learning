8f7272d22e20ba8c3ed14a1789878d9e08a8292a
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-13 18:43:35 +0200

    Append rows in case of axis=0

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,8 +9,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1[0]) != len(mat2[0]):
             return None
         else:
-            mat.append(mat1)
-            mat.append(mat2)
+            for row in mat1:
+                mat.append(row)
+            for row in mat2:
+                mat.append(row)
             return mat
     elif axis == 1:
         if len(mat1) != len(mat2):
@@ -18,6 +20,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
         else:
             mat = list(map(lambda row: row[0] + row[1], zip(mat1, mat2)))
             return mat
-            
-
-        
