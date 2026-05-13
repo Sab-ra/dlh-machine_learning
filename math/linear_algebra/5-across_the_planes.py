1c95868dfb5765efad827b3c316d434156e81a52
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-13 16:00:10 +0200

    Adjust checks to return [], from empty matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,13 +3,14 @@
 
 def add_matrices2D(mat1, mat2):
     """Element-wize"""
+    matrix = []
     msh = __import__('2-size_me_please')
-    if len(mat1) == 0:
-        return None
+    if mat1 == [] and mat2 == []:
+        return matrix
     if msh.matrix_shape(mat1) != msh.matrix_shape(mat2):
         return None
     else:
         matrix = []
         for row1, row2 in zip(mat1, mat2):
             matrix.append([x + y for x, y in zip(row1, row2)])
-        return matrix
+    return matrix
