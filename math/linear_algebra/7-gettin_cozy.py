4f12f5f500604d546d216d76f8f50157979c3a5a
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-13 18:28:53 +0200

    Do list(map(lambda ..., zip()))

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Concatenate Matrices"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Axis=0: stash, Axis=1: side_by_side"""
+    mat = []
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        else:
+            mat.append(mat1)
+            mat.append(mat2)
+            return mat
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            mat = list(map(lambda row: row[0] + row[1], zip(mat1, mat2)))
+            return mat
+            
+
+        
