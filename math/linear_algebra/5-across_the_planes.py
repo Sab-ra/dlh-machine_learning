e6be9943c314f699dc5b7408af910e4ef7222c22
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-13 17:16:59 +0200

    Another try

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,16 +3,13 @@
 
 def add_matrices2D(mat1, mat2):
     """Element-wize"""
-    matrix = []
-    msh = __import__('2-size_me_please')
-    if mat1 == [] and mat2 == []:
-        return []
-    if mat1 == [] or mat2 == []:
-        return None
-    if msh.matrix_shape(mat1) != msh.matrix_shape(mat2):
-        return None
-    else:
+
+    try:
         matrix = []
         for row1, row2 in zip(mat1, mat2):
             matrix.append([x + y for x, y in zip(row1, row2)])
         return matrix
+    except IndexError:
+        return None
+    except TypeError:
+        return []
