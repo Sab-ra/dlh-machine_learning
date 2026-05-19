4e383a36481f321c7fed76cecf9005461b374c09
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-19 11:38:30 +0200

    Improve code for task 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,12 +4,12 @@
 def add_matrices2D(mat1, mat2):
     """Element-wize"""
 
-    try:
-        matrix = []
-        for row1, row2 in zip(mat1, mat2):
-            matrix.append([x + y for x, y in zip(row1, row2)])
-        return matrix
-    except IndexError:
+    if len(mat1) != len(mat2):
         return None
-    except TypeError:
-        return []
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    matrix = []
+    for row1, row2 in zip(mat1, mat2):
+        matrix.append([x + y for x, y in zip(row1, row2)])
+    return matrix
