1ea708b964f5a9a2494949504b0ff8f64804981b
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-19 13:33:54 +0200

    Remove 13 completely

--- math/linear_algebra/13-cats_got_your_tongue.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Concatenate matrices"""
-import numpy as np
-
-
-def np_cat(mat1, mat2, axis=0):
-    """Call with numpy imported"""
-    return np.concatenate((mat1, mat2), axis=axis)
