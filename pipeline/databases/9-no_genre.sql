62363e53d2865b38e481fc9d58db94f5c86a274f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-22 14:35:58 +0200

    Typos

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,7 +1,7 @@
 -- Select no ganre shows
 SELECT 
-tv_shows.title AS title
-tv_show_genres.id AS genre_id
+tv_shows.title AS title,
+tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
