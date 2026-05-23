7f0ed481e8390692c065276798c03ce86bf2d9ae
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-23 22:54:18 +0200

    light metal

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,9 +1,7 @@
 -- country fans
 SELECT
-o.origin AS origin,
-SUM(b.fans) AS nb_fans
-FROM metal_bands AS o
-JOIN metal_bands AS b
-ON o.band_name = b.band_name
+origin AS origin,
+SUM(fans) AS nb_fans
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
