2de3454744f2e46654352e99236f27f6edcf2389
sabra <119448985+Sab-ra@users.noreply.github.com>
2026-05-27 12:43:38 +0000

    Style

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -11,9 +11,8 @@ def determinant(matrix):
     scale = 1
 
     # validate matrix variable
-    if not isinstance(matrix, list) or \
-        not all(isinstance(row, list) for row in matrix):
-            raise TypeError("matrix must be a list of lists")
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
     if not all(len(row) == n for row in matrix):
         if matrix == [[]]:
             return 1
