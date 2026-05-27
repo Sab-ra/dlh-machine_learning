06ba3832e4dbd91680f91106bc74d73edde91865
sabra <119448985+Sab-ra@users.noreply.github.com>
2026-05-27 11:31:51 +0000

    Determinant by Gaussan

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Calc Determinant by Gaussan Elim"""
+
+
+def determinant(matrix):
+    """Preseve integers on computation"""
+    # validate matrix variable
+    if not isinstance(matrix, list) or\
+        not all(isinstance(row, list)\
+                for row in matrix):
+        raise TypeError\
+            ("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        raise ValueError\
+            ("matrix must be a square matrix")
+    if matrix == [[]]:
+        return 0
+    
+    # variables:
+    n = len(matrix)
+    A = [row[:] for row in matrix]
+    det = 1
+    scale = 1
+
+    for i in range(n):
+        if A[i][i] == 0:
+            for k in range(i+1, n):
+                if A[k][i] != 0:
+                    A[i], A[k] = A[k], A[i]
+                    det *= -1
+                    break
+            else:
+                return 0
+        pivot = A[i][i]
+
+        for j in range(i+1, n):
+            if A[j][i] != 0:
+                target = A[j][i]
+
+                for k in range(i, n):
+                    A[j][k] = pivot * A[j][k] - target * A[i][k]
+
+                scale *= pivot
+
+    for i in range(n):
+        det *= A[i][i]
+    return det // scale
