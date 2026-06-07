cfe894a14b9ffdb51f05bb06c3f958ad3848b44e
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-06-07 12:30:01 +0200

    Fix style

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+"""Plotting a line graph with Matplotlib"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
