71861ba5fc9ce3c8f1938127aa36e4ac2c52987a
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-06-07 12:44:06 +0200

    Spaces

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -7,11 +7,9 @@ import matplotlib.pyplot as plt
 def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
     plt.plot(y, color = "red")
     plt.xlim(0, 10)
     plt.savefig('line.png')
     plt.show()
-
 if __name__ == '__main__':
     line()
