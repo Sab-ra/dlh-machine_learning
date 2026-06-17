b41116ff6fcba469faeaa46b591a3c674f0465df
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-06-17 23:16:39 +0200

    Shortenned longest line

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -13,12 +13,15 @@ def factorial(i):
             result *= j
         return result
 
+
 def likelihood(x, n, P):
     """likelihood function"""
+
+    verl = "x must be an integer that is greater than or equal to 0"
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(verl)
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
