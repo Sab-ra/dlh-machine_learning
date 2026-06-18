ac98c897540ee1a80973adf563dd349204e6e2fa
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-06-18 13:12:54 +0200

    Style

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -7,16 +7,18 @@ def intersection(x, n, P, Pr):
     """Calculates the intersection of obtaining
     data with hypothetical probabilities"""
 
+    xnv = 'x must be an integer that is greater than or equal to 0'
+    prt = 'Pr must be a numpy.ndarray with the same shape as P'
     if not isinstance(n, int) or n <= 0:
         raise ValueError('n must be a positive integer')
     if not isinstance(x, int) or x < 0:
-        raise ValueError('x must be an integer that is greater than or equal to 0')
+        raise ValueError(xnv)
     if x > n:
         raise ValueError('x cannot be greater than n')
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
-        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+        raise TypeError(prt)
     var_p = [P, Pr]
     for i in range(2):
         name_domain = ['P', 'Pr']
@@ -28,6 +30,7 @@ def intersection(x, n, P, Pr):
         raise ValueError('Pr must sum to 1')
     
     return likelihood(x, n, P) * Pr
+
 
 def factorial(i):
     """calculate factorial"""
