aa88512b36b4bf31b48ed59f604f0ee5620f864f
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-06-18 12:10:19 +0200

    Not import any module bub np

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -22,6 +22,34 @@ def intersection(x, n, P, Pr):
         raise ValueError(' the range [0, 1], raise a ValueError with the message All values in {var_p} must be in the range [0, 1]')
     if not numpy.isclose(Pr, 1):
         raise ValueError('Pr must sum to 1')
+    
+    return likelihood(x, n, P) * Pr
 
-    likelihood = __import__('0-likelihood').likelihood(x, n, P)
-    return likelihood * Pr
+def factorial(i):
+    """calculate factorial"""
+    if i == 0:
+        return 1
+    else:
+        result = 1
+        for j in range(1, i+1):
+            result *= j
+        return result
+
+
+def likelihood(x, n, P):
+    """likelihood function"""
+
+    verl = "x must be an integer that is greater than or equal to 0"
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(verl)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    comb = factorial(n) / (factorial(x) * factorial(n - x))
+    return comb * P ** x * (1 - P) ** (n - x)
