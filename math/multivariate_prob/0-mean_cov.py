c8fa3b6aacde21ea1a1e39517ca15b8ef5db8164
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-06-26 16:29:19 +0200

    Style

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -10,15 +10,15 @@ def mean_cov(X):
     x_ter = f'X must be a 2D numpy.ndarray'
     x_ver = f'X must contain multiple data points'
 
-    if not isinstance(X, np.ndarray) or len(X.shape) !=2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError(x_ter)
     # n = number of trials, d = number of dimensions (rank, die, etc.)
     n, d = X.shape
     if n < 2:
         raise ValueError(x_ver)
-    
+
     # 1. Calculate the Mean Vector (mu)
-    # We take the average of each column. 
+    # We take the average of each column.
     # keepdims=True ensures the shape is (1, d) instead of just (d,)
     mean = np.mean(X, axis=0, keepdims=True)
 
@@ -27,10 +27,9 @@ def mean_cov(X):
     X_centered = X - mean
 
     # Then, perform the "Product of Differences" for all dimensions at once.
-    # Matrix multiplication (@) of (X-mu).T and (X-mu) handles the 
+    # Matrix multiplication (@) of (X-mu).T and (X-mu) handles the
     # (Rank - mu_rank) * (Die - mu_die) math for every pair.
-    # We divide by n to get the average, as used in your manual 5-trial example.
+    # We divide by n to get the average, as used in the manual 5-trial example.
     cov = (X_centered.T @ X_centered) / (n - 1)
 
     return mean, cov
-    
