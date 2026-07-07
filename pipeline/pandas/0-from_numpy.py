f854cfc8ff42d25bab64dfbc2b83f951ba65cc47
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-07-07 19:46:21 +0200

    Blank line

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def from_numpy(array):
     """columns will be labled a-z order"""
-    
+
     col_count = array.shape[1]
     col_titles = [chr(i) for i in range(65, 65 + col_count)]
 
