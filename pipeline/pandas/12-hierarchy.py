9e77fd40b3a0790f1a8e0b4d899e2db33dc316a3
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-07-08 15:19:56 +0200

    Swap sets

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -5,11 +5,16 @@ import pandas as pd
 
 def hierarchy(df1, df2):
     """MultiIndex"""
+
     index = __import__('10-index').index
+
     df1 = index(df1)
     df2 = index(df2)
+
     df1_filtered = df1.loc[1417411980:1417417980]
     df2_filtered = df2.loc[1417411980:1417417980]
-    df = pd.concat([df1_filtered, df2_filtered],
+
+    df = pd.concat([df2_filtered, df1_filtered],
                    keys=['bitstamp', 'coinbase'])
+
     return df.swaplevel(0, 1).sort_index()
