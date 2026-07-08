7e9068719b53441e04de589cf3cc86b95f2b2a45
Sabra <119448985+Sab-ra@users.noreply.github.com>
2026-07-08 13:33:42 +0200

    Concatenate filtered DF's

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Concatenate 2 DF's"""
+
+
+def concat(df1, df2):
+    """Base on Timestamp as index"""
+
+    index = __import__('10-index').index
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2_filtered = df2.loc[:1417411920]
+    df = pd.concat([df2_filtered, df1], keys=['bitstamp', 'coinbase'])
+
+    return df
